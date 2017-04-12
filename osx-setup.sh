dff0fd64af1d36aaedfd5f7fa0ce876a8d15fba2
Christopher <chris@planner.net.br>
2017-04-12 17:06:45 +0200

    Create osx-setup.sh

---
+++ osx-setup.sh
@@ -0,0 +1,10 @@
+# make sure you have docker for mac installed before
+# https://docs.docker.com/docker-for-mac/install/#download-docker-for-mac
+
+# install and setup the xhyve driver
+brew install docker-machine-driver-xhyve
+sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
+sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
+
+# install minikube
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.18.0/minikube-darwin-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
